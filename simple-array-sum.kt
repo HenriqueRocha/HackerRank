a50af62621e1cfa947b0eee32ee9b5881d4cbd6a
Henrique Rocha <henrique.rocha@proton.me>
2022-07-03 22:10:13 +0200

    Solution for Simple Array Sum

---
+++ simple-array-sum.kt
@@ -0,0 +1,9 @@
+// https://www.hackerrank.com/challenges/simple-array-sum
+fun simpleArraySum(ar: Array<Int>): Int {
+    var result = 0
+    for (n in ar) {
+        result += n
+    }
+    return result
+
+}
